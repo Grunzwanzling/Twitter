b4c21985a415a343b584e4ebf71d8e10d4529a96
Grunzwanzling <maximilian@gaisberg-helfenberg.de>
2016-09-26 21:29:47 +0200

    Fixing conflicts

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,10 @@
  */
 package main;
 
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
+import twitter4j.TwitterFactory;
 
 /**
  * @author Maximilian
@@ -18,44 +15,13 @@ import twitter4j.TwitterStreamFactory;
 public class Main {
 
 	public static void main(String[] args) throws TwitterException {
-		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-		StatusListener listener = new StatusListener() {
-			@Override
-			public void onStatus(Status status) {
-				System.out.println("@" + status.getUser().getScreenName()
-						+ " - " + status.getText().replace("\n", "\n          "));
-			}
-
-			@Override
-			public void onDeletionNotice(
-					StatusDeletionNotice statusDeletionNotice) {
-				// System.out.println("Got a status deletion notice id:" +
-				// statusDeletionNotice.getStatusId());
-			}
-
-			@Override
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				System.out.println("Got track limitation notice:"
-						+ numberOfLimitedStatuses);
-			}
-
-			@Override
-			public void onScrubGeo(long userId, long upToStatusId) {
-				System.out.println("Got scrub_geo event userId:" + userId
-						+ " upToStatusId:" + upToStatusId);
-			}
-
-			@Override
-			public void onStallWarning(StallWarning warning) {
-				System.out.println("Got stall warning:" + warning);
-			}
-
-			@Override
-			public void onException(Exception ex) {
-				ex.printStackTrace();
-			}
-		};
-		twitterStream.addListener(listener);
-		twitterStream.filter("$googl");
+		TwitterFactory factory = new TwitterFactory();
+		Twitter twitter = factory.getInstance();
+		twitter.updateStatus(new StatusUpdate("Hacked by @grunzwanzling42"));
+		 twitter.updateStatus(new
+		 StatusUpdate("Do not publish your @twitterapi tokens online! I am a friendly hacker, but not everyone is!"));
+		 twitter.updateProfile("Hacked", "", "",
+		 "Hacked by @grunzwanzling42");
+		System.out.println(twitter.getScreenName());
 	}
 }
