956551560319a36c0c2eadde03b30f9c5dc53276
Grunzwanzling <maximilian@gaisberg-helfenberg.de>
2016-10-17 19:17:14 +0200

    Improved ToneAnalyzer

--- src/main/ToneAnalyzer.java
+++ src/main/ToneAnalyzer.java
@@ -7,11 +7,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Properties;
 
-import essentials.Essentials;
-import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -19,6 +19,7 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
+import essentials.Essentials;
 
 /**
  * @author Maximilian
@@ -31,31 +32,56 @@ public class ToneAnalyzer {
 			"conscientiousness", "extraversion", "agreeableness",
 			"emotional_range" };
 
-	private int[] topThree(float[] array) {
+	private int[] topFive(float[] array) {
 		float max1 = Float.MIN_VALUE;
 		float max2 = Float.MIN_VALUE;
 		float max3 = Float.MIN_VALUE;
+		float max4 = Float.MIN_VALUE;
+		float max5 = Float.MIN_VALUE;
 
 		int i1 = -1;
 		int i2 = -1;
 		int i3 = -1;
+		int i4 = -1;
+		int i5 = -1;
 
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > max1) {
+				max5 = max4;
+				max4 = max3;
 				max3 = max2;
 				max2 = max1;
 				max1 = array[i];
+				i5 = i4;
+				i4 = i3;
 				i3 = i2;
 				i2 = i1;
 				i1 = i;
 			} else if (array[i] > max2) {
+				max5 = max4;
+				max4 = max3;
 				max3 = max2;
 				max2 = array[i];
+				i5 = i4;
+				i4 = i3;
 				i3 = i2;
 				i2 = i;
 			} else if (array[i] > max3) {
+				max5 = max4;
+				max4 = max3;
 				max3 = array[i];
+				i5 = i4;
+				i4 = i3;
 				i3 = i;
+
+			} else if (array[i] > max4) {
+				max5 = max4;
+				max4 = array[i];
+				i5 = i4;
+				i4 = i;
+			} else if (array[i] > max5) {
+				max5 = array[i];
+				i5 = i;
 			}
 		}
 		int[] result = { i1, i2, i3 };
@@ -71,9 +97,9 @@ public class ToneAnalyzer {
 		AccessToken accessToken = loadAccessToken("abos");
 		twitter = factory.getInstance();
 		twitter.setOAuthAccessToken(accessToken);
-		float[] t = check("#jugendhackt");
-		System.out.println("#jugendhackt");
-		for (int i : topThree(t)) {
+		float[] t = check("#HillaryBecause");
+		System.out.println("#HillaryBecause");
+		for (int i : topFive(t)) {
 			System.out.println(tone_ids[i] + " " + t[i]);
 		}
 	}
@@ -83,15 +109,16 @@ public class ToneAnalyzer {
 		try {
 			Query query = new Query(hashtag);
 			QueryResult result;
-			query.setCount(100);
+			query.setCount(1);
 			result = twitter.search(query);
 
 			List<Status> statusList = result.getTweets();
 			String text = "";
 			for (Status status : statusList) {
 
-				text = status.getText() + "\n";
+				text = text + status.getText() + "\n";
 			}
+			// System.out.println(text);
 			scores = analyzeTone(text);
 			// for (int i = statusList.size(); i >= 0; i--) {
 			// twitter.createFavorite(statusList.get(1).getId());
@@ -104,10 +131,13 @@ public class ToneAnalyzer {
 	}
 
 	private static float[] analyzeTone(String message) throws IOException {
-		String request = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=";
+		URL url = new URL(
+				"https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone/?version=2016-05-19&text="
+						+ message);
+		// params = URLEncoder.encode(params);
+		// params = "test";
 		try {
-			String result = Essentials.sendHTTPRequest(
-					Essentials.escapeURL(request + message),
+			String result = Essentials.sendHTTPRequest(url,
 					"28d2ffc3-e805-470c-99f8-c4062ecb2569", "MGdeoIyimoVU");
 			float[] values = new float[13];
 			int start = 0;
